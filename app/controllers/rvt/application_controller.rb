9e21322b6fe1b669f912002927785dfdc3af8d1b
Valentin Gololobov <mails.valentin@gmail.com>
2018-04-12 22:03:41 +0300

    removed whitelist

--- app/controllers/rvt/application_controller.rb
+++ app/controllers/rvt/application_controller.rb
@@ -1,6 +1,5 @@
 module RVT
   class ApplicationController < ActionController::Base
-    before_action :prevent_unauthorized_requests!
     http_basic_authenticate_with :name => "sensconsole", :password => "Toquee2wioza" 
 
   end
